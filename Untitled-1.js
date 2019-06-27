6f47c35448566a43d825807f765d4f5517a194ad
Janek <antarcticdepths@gmail.com>
2019-06-27 12:35:50 +0200

    make a small dot instead of large one

--- Untitled-1.js
+++ Untitled-1.js
@@ -3,13 +3,13 @@ let pad = document.getElementById('pad');
 function handleMouseDown(e) {
   var container = document.createElementNS("http://www.w3.org/2000/svg", 'svg');
     container.setAttribute('display', 'block');
-    container.setAttribute("width", "100");
-    container.setAttribute("height", "100");
+    container.setAttribute("width", "1");
+    container.setAttribute("height", "1");
 
     const cir1 = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-      cir1.setAttribute("cx", "80");
-      cir1.setAttribute("cy", "80");
-      cir1.setAttribute("r", "30");
+      cir1.setAttribute("cx", "0");
+      cir1.setAttribute("cy", "0");
+      cir1.setAttribute("r", "1");
       cir1.setAttribute("fill", "black");
     container.appendChild(cir1);
 
