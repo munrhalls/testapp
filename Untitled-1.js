2350b59bbf98f1955a636ba2d7af34dc02503052
Janek <antarcticdepths@gmail.com>
2019-06-27 13:54:16 +0200

    place dot w/o svg or canvas

--- Untitled-1.js
+++ Untitled-1.js
@@ -1,17 +1,19 @@
 let pad = document.getElementById('pad');
 
 function handleMouseDown(e) {
-  var container = document.createElementNS("http://www.w3.org/2000/svg", 'svg');
-    container.setAttribute('display', 'block');
-    container.setAttribute("width", "1");
-    container.setAttribute("height", "1");
-
-    const cir1 = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-      cir1.setAttribute("cx", "0");
-      cir1.setAttribute("cy", "0");
-      cir1.setAttribute("r", "1");
-      cir1.setAttribute("fill", "black");
-    container.appendChild(cir1);
+  var container = document.createElement('div');
+    container.style.width = "1px";
+    container.style.height = "1px";
+    container.style.padding = "0";
+    container.style.position = 'absolute';
+    container.style.top = e.clientY + 'px';
+    container.style.left = e.clientX + 'px';
+    // const cir1 = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    //   cir1.setAttribute("cx", "0");
+    //   cir1.setAttribute("cy", "0");
+    //   cir1.setAttribute("r", "1");
+    //   cir1.setAttribute("fill", "black");
+    // container.appendChild(container);
 
   pad.appendChild(container);
 }
